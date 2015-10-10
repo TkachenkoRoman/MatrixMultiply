11b1a5cc1aa973ce861bc6233b3d0405b4f2f98d
Ткаченко Роман Юриевич <tkachenkoromanury@gmail.com>
2015-10-11 00:36:36 +0300

    initial commit

---
+++ MatrixMultiply/MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatrixMultiply
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MatrixMultiplier matrixMultiplier;
+
+        public MainWindow()
+        {
+            InitMatrixMultiplier();
+            InitializeComponent();        
+        }
+
+        private void InitMatrixMultiplier()
+        {
+            matrixMultiplier = new MatrixMultiplier();
+        }
+
+        private async void ButtonCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var watch = Stopwatch.StartNew();
+
+                int[,] res = await matrixMultiplier.MultiplyAsync();
+
+                watch.Stop();
+                var elapsedMs = watch.ElapsedMilliseconds;
+                LabelDefaultTime.Content = elapsedMs.ToString();
+                //
+                // Print res matrix in Output
+                //
+                //for (int i = 0; i < res.GetLength(0); i++)
+                //{
+                //    for (int j = 0; j < res.GetLength(1); j++)
+                //    {
+                //        Debug.Write(res[i, j].ToString() + " ");
+                //    }
+                //    Debug.WriteLine("");
+                //}
+            }
+            catch (AggregateException ae)
+            {
+                MessageBox.Show("Wrong matrix sizes");
+                //Debug.WriteLine("*** ERROR *** " + ex.Message);
+            }
+        }
+
+        #region TextBoxes TextChanging handling
+        private void TextBoxMatr1Rows_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int amount = 0;
+            try
+            {
+                amount = Int32.Parse(TextBoxMatr1Rows.Text);
+            }
+            catch
+            {
+                MessageBox.Show("This parameter must be positive integer");
+                return;
+            }
+            matrixMultiplier.Matr1RowsAmount = amount;
+        }
+
+        private void TextBoxMatr1Cols_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int amount = 0;
+            try
+            {
+                amount = Int32.Parse(TextBoxMatr1Cols.Text);
+            }
+            catch
+            {
+                MessageBox.Show("This parameter must be positive integer");
+                return;
+            }
+            matrixMultiplier.Matr1ColsAmount = amount;
+        }
+
+        private void TextBoxMatr2Rows_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int amount = 0;
+            try
+            {
+                amount = Int32.Parse(TextBoxMatr2Rows.Text);
+            }
+            catch
+            {
+                // skipping this message cause TextBoxMatr2Rows.Text is bind to TextBoxMatr1Cols.Text
+                //MessageBox.Show("This parameter must be integer"); 
+                return;
+            }
+            matrixMultiplier.Matr2RowsAmount = amount;
+        }
+
+        private void TextBoxMatr2Cols_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int amount = 0;
+            try
+            {
+                amount = Int32.Parse(TextBoxMatr2Cols.Text);
+            }
+            catch
+            {
+                MessageBox.Show("This parameter must be positive integer");
+                return;
+            }
+            matrixMultiplier.Matr2ColsAmount = amount;
+        }
+        #endregion
+    }
+}
---
+++ MatrixMultiply/MatrixMultiplier.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MatrixMultiply
+{
+    class MatrixMultiplier
+    {
+        public int Matr1RowsAmount { get; set; }
+        public int Matr1ColsAmount { get; set; }
+        public int Matr2RowsAmount { get; set; }
+        public int Matr2ColsAmount { get; set; }
+
+        private Random random;
+        private int[,] matr1;
+        private int[,] matr2;
+
+        public MatrixMultiplier()
+        {
+            random = new Random();
+        }
+
+        public bool InitRandomMatrix()
+        {
+            if (Matr1RowsAmount > 0 && Matr2RowsAmount > 0 
+                && Matr1ColsAmount > 0 && Matr2ColsAmount > 0)
+            {
+                matr1 = new int[Matr1RowsAmount, Matr1ColsAmount];
+                matr2 = new int[Matr2RowsAmount, Matr2ColsAmount];
+
+                for (int i = 0; i < Matr1RowsAmount; i++)
+                {
+                    for (int j = 0; j < Matr1ColsAmount; j++)
+                    {
+                        matr1[i, j] = random.Next(0, 100); /// TODO create min max random props in interface
+                    }
+                }
+
+                for (int i = 0; i < Matr2RowsAmount; i++)
+                {
+                    for (int j = 0; j < Matr2ColsAmount; j++)
+                    {
+                        matr2[i, j] = random.Next(0, 100); 
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<int[,]> MultiplyAsync()
+        {
+            int[,] res = await Task<int[,]>.Factory.StartNew(
+                () => Multiply(), 
+                CancellationToken.None,
+                TaskCreationOptions.None,
+                TaskScheduler.Default);
+            return res;               
+        }
+
+        public int[,] Multiply()
+        {
+            if (!InitRandomMatrix())
+            {
+                throw new Exception("Specify valid matrix sizes");
+            }
+            if (matr1 != null && matr2 != null && matr1.GetLength(1) != matr2.GetLength(0))
+            {
+                throw new Exception("Matrix1 cols amount must be equal to matrix2 rows amount");
+            }
+            int[,] res = new int[matr1.GetLength(0), matr2.GetLength(1)];
+            for (int i = 0; i < matr1.GetLength(0); i++)
+            {
+                for (int j = 0; j < matr2.GetLength(1); j++)
+                {
+                    for (int k = 0; k < matr2.GetLength(0); k++)
+                    {
+                        res[i, j] += matr1[i, k] * matr2[k, j];
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
